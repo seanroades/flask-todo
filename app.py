51d3e6e01421f2380e730325851837237114eed3
Sean Roades <seanroades@college.harvard.edu>
2023-02-10 16:03:06 -0500

    Comments

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask, redirect, request, render_template
+from flask import Flask, redirect, request, render_template # Import manner
 
 app = Flask(__name__)
 
 todos = []
 
-@app.route("/")
+@app.route("/") # Routing
 def index():
-    return render_template("index.html", todos=todos)
+    return render_template("index.html", todos=todos) # Page rendering
 
 @app.route("/add", methods=["POST"])
 def add():
-    todo = request.form.get("todo")
+    todo = request.form.get("todo") # request.get 
     todos.append(todo)
-    return redirect("/")
+    return redirect("/") # redirect  # Pythonic functions 
 
 if __name__ == "__main__":
     app.run()
